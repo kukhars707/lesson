10d74b2f352d1675218113f131ea32ff22128f00
Sergey <kukhar.s707@gmail.com>
2018-04-02 22:14:29 +0300

    file db

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -4,12 +4,11 @@ import { StaticDataSource } from "./static.datasource";
 import { Cart } from "./cart.model";
 import { Order } from "./order.model";
 import { OrderRepository } from "./order.repository";
-import { RestDataSource } from "./rest.datasource";
+// import { RestDataSource } from "./rest.datasource";
 import { HttpModule } from "@angular/http";
 
 @NgModule({
     imports: [HttpModule],
-    providers: [ProductRepository, Cart, Order, OrderRepository,
-        { provide: StaticDataSource, useClass: RestDataSource }]
+    providers: [ProductRepository, Cart, Order, OrderRepository, StaticDataSource]
 })
 export class ModelModule { }
